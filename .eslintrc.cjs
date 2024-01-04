ee8df1488e2d37a65ea53b662bb468328e7ae8a7
Emanuel Peire <emapeire@outlook.com>
2024-01-04 17:45:54 -0300

    Update .eslintrc.cjs configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,29 @@
 /** @type {import("eslint").Linter.Config} */
+
 const config = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-refresh"],
   extends: [
+    "eslint:recommended",
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -31,7 +39,18 @@ const config = {
         checksVoidReturn: { attributes: false },
       },
     ],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/jsx-no-target-blank": [2, { allowReferrer: false }],
+    "react/prop-types": "off",
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
 };
 
 module.exports = config;
